cae5152967d44c1283862df79f0e3bd42da4f7ec
yennisef <90271148+yennisef@users.noreply.github.com>
2021-09-17 10:31:18 +0700

    Add files via upload

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+
+    public float xInitialForce;
+    public float yInitialForce;
+
+    private Vector2 trajectoryOrigin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        RestartGame();
+
+        trajectoryOrigin = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ResetBall()
+    {
+        transform.position = Vector2.zero;
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        //float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        float randomDirection = Random.Range(0, 2);
+
+        if (randomDirection < 1.0f)
+        {
+            //rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
+        }
+    }
+
+    void RestartGame()
+    {
+        ResetBall();
+
+        Invoke("PushBall", 2);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+}
---
+++ Assets/Scripts/playercontrol.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public KeyCode downButton = KeyCode.S;
+    public KeyCode upButton = KeyCode.W;
+
+    public float speed = 10.0f;
+
+    public float yBoundary = 9.0f;
+
+    private Rigidbody2D rigidBody2D;
+
+    private int score;
+
+    //titik tumbukan terakhir dengan bola, untuk menampilkan variabel-variabel fisika terkait tumbukan tersebut
+    private ContactPoint2D lastContactPoint;
+
+    //untuk mengakses informasi titik kontak dari kelas lain
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = rigidBody2D.velocity;
+
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+        }
+
+        rigidBody2D.velocity = velocity;
+
+        Vector3 position = transform.position;
+
+        if (position.y > yBoundary)
+        {
+            position.y = yBoundary;
+        }
+        else if (position.y < -yBoundary)
+        {
+            position.y = -yBoundary;
+        }
+
+        transform.position = position;
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    public int Score
+    {
+        get { return score; }
+    }
+
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+}
